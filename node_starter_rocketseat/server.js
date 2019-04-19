3a32296d5619c1ddac8a6e6317edc24f8f1cedfb
Andre Marcelino <marcelinoandre@gmail.com>
2019-04-18 23:38:41 -0300

    Servidor node + mongo db

---
+++ node_starter_rocketseat/server.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
+
+//iniciando o App
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/cursonode', {
+    useNewUrlParser: true
+});
+
+requireDir ('./src/models');
+app.use('/api', require('./src/routes'));
+
+
+app.listen(3001);
+console.log('server rodando na porta 3001')
---
+++ node_starter_rocketseat/src/models/Product.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const ProductShema = new mongoose.Schema({
+
+    title :{
+        type : String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+
+    url:{
+        type: String,
+        required: true,
+    },
+
+    createAt:{
+        type: Date,
+        default: Date.now
+    }
+
+})
+ProductShema.plugin(mongoosePaginate)
+mongoose.model('Product', ProductShema);
